1719a5796a7f53ebfe53591fdde2aad6c3f8ba0d
Sultan Mansour Raofi <sultanmansoor40@gmail.com>
2025-05-04 15:43:10 +0430

    Controller

---
+++ src/main/java/controllers/TodoController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import com.sultanmansoor40.todolistproject.entities.Todo;
+import com.sultanmansoor40.todolistproject.entities.TodoRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/todos")
+public class TodoController {
+    private final TodoRepository todoRepository;
+    public TodoController(TodoRepository todoRepository){
+        this.todoRepository = todoRepository;
+    }
+
+    @PostMapping
+    public Todo newTodo(@RequestBody Todo todo){
+        return  this.todoRepository.save(todo);
+    }
+}
