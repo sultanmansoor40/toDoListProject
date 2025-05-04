70f33ee6ec9186c8bddf93e85d25d523b74fc6a8
Sultan Mansour Raofi <sultanmansoor40@gmail.com>
2025-05-04 15:51:59 +0430

    fetching data

--- src/main/java/com/sultanmansoor40/todolistproject/controllers/TodoController.java
+++ src/main/java/com/sultanmansoor40/todolistproject/controllers/TodoController.java
@@ -2,10 +2,10 @@ package com.sultanmansoor40.todolistproject.controllers;
 
 import com.sultanmansoor40.todolistproject.entities.Todo;
 import com.sultanmansoor40.todolistproject.entities.TodoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/todos")
@@ -15,8 +15,19 @@ public class TodoController {
         this.todoRepository = todoRepository;
     }
 
+    @GetMapping
+    public List<Todo> getTodos(){
+        return todoRepository.findAll();
+    }
+
+    @GetMapping("/{todoId}")
+    public Optional<Todo> getTodo(@PathVariable("todoId") Long todoId){
+        return todoRepository.findById(todoId);
+    }
+
     @PostMapping
     public Todo newTodo(@RequestBody Todo todo){
         return  this.todoRepository.save(todo);
     }
+
 }
