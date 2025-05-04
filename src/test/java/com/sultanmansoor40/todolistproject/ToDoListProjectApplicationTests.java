1d8c6b8d9a2d03396b1eabdcb45f657f49c51fcb
Sultan Mansour Raofi <sultanmansoor40@gmail.com>
2025-05-04 14:52:58 +0430

    Initial commit

---
+++ src/test/java/com/sultanmansoor40/todolistproject/ToDoListProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sultanmansoor40.todolistproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
